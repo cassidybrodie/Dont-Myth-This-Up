c5921905122102f6606a99b2c38a781d547fb006
Cassidy <60491956+cassidybrodie@users.noreply.github.com>
2021-04-08 12:34:35 -0700

    Initial

---
+++ Assets/Clicker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(isBeingHeld == true)
+        {
+
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+            isBeingHeld = true;
+
+            print(gameObject.name);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+
+
+    }
+}
---
+++ Assets/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("Switching Scene");
+    }
+}
